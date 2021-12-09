281777417539ad77cfb447725201f7f847ac97d6
Sheehan Olver <solver@mac.com>
2021-12-09 14:18:09 +0000

    some updates

--- ForwardDiffSchrodingerExample.jl
+++ ForwardDiffSchrodingerExample.jl
@@ -1,14 +1,24 @@
 using ForwardDiff, LinearAlgebra
 import ForwardDiff: derivative, gradient, hessian, Dual, jacobian
 
-function banded_det(x) # x is a vector of polynomial coefficients
+function banded_eigvals(x) # x is a vector of polynomial coefficients
     range = -10.0:10
-    f = zeros(length(range))
+    f = zeros(eltype(x), length(range))
     for i = 1:length(x)
-        f .+= x[i]*range.^(i-1)
+        f .+= x[i] .* range .^ (i - 1)
     end
-    M = SymTridiagonal(f, ones(length(range)-1))
-    det(M) 
+    M = SymTridiagonal(f, ones(eltype(x), length(range) - 1))
+    eigvals(M)
 end
 
-jacobian(banded_det, [0,1,2])
+banded_eigvals([0.0, 1, 2])
+
+jacobian(banded_eigvals, [0.0, 1, 2])
+
+(banded_eigvals([0.0, 1 + h, 2]) - banded_eigvals([0.0, 1, 2])) / h
+
+h = 0.00001
+(banded_det([0 + h, 1, 2]) - banded_det([0, 1, 2])) / h
+(banded_det([0, 1 + h, 2]) - banded_det([0, 1, 2])) / h
+(banded_det([0, 1, 2 + h]) - banded_det([0, 1, 2])) / h
+
