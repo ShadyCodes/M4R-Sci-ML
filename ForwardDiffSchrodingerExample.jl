4f355a075fda4cd8f02bc510ec7d69eb67e22ba5
Dharshan B <sdbkar23@gmail.com>
2021-12-09 12:52:31 +0000

    First attempt at using autodiff

---
+++ ForwardDiffSchrodingerExample.jl
@@ -0,0 +1,14 @@
+using ForwardDiff, LinearAlgebra
+import ForwardDiff: derivative, gradient, hessian, Dual, jacobian
+
+function banded_det(x) # x is a vector of polynomial coefficients
+    range = -10.0:10
+    f = zeros(length(range))
+    for i = 1:length(x)
+        f .+= x[i]*range.^(i-1)
+    end
+    M = SymTridiagonal(f, ones(length(range)-1))
+    det(M) 
+end
+
+jacobian(banded_det, [0,1,2])
