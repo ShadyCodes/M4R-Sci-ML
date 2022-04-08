1a5d2d2ce95c1f2ff8e5a2f38a39b9413d7f50d1
Dharshan B <sdbkar23@gmail.com>
2022-04-08 17:01:16 +0100

    successful rrule implemented

--- SymTridiagonalRRules.jl
+++ SymTridiagonalRRules.jl
@@ -1,4 +1,4 @@
-using Zygote, LinearAlgebra, ChainRules, ChainRulesCore
+using Zygote, LinearAlgebra, ChainRules, ChainRulesCore, ChainRulesTestUtils
 
 function ChainRulesCore.rrule(::Type{SymTridiagonal}, dv::AbstractVector, ev::AbstractVector)
     SymTridiagonal_pullback(Δdv, Δev) = NoTangent(), SymTridiagonal(Δdv, Δev)
@@ -6,12 +6,18 @@ function ChainRulesCore.rrule(::Type{SymTridiagonal}, dv::AbstractVector, ev::Ab
 end
 
 
-A = c -> Symmetric([c[1] c[1]; 0 c[2]+2c[3]]);
-λ = c -> eigvals(A(c))
+#A = c -> Symmetric([c[1] c[1]; 0 c[2]+2c[3]]);
+#λ = c -> eigvals(A(c))
 
-Zygote.jacobian(λ, [0.1,0.2,0.3])
+#Zygote.jacobian(λ, [0.1,0.2,0.3])
 
 
-A = c -> SymTridiagonal([c[1],c[1],c[2]+2c[3], c[2]], ones(3));
-λ = c -> eigvals(A(c))
-Zygote.jacobian(A, [0.1,0.2,0.3])
+#A = c -> SymTridiagonal([c[1],c[1],c[2]+2c[3], c[2]], ones(3));
+#λ = c -> eigvals(A(c))
+#Zygote.jacobian(A, [0.1,0.2,0.3]
+
+function ChainRulesCore.frule((_, Δdv, Δev), ::Type{SymTridiagonal}, dv::AbstractVector, ev::AbstractVector)
+    return SymTridiagonal(dv,ev), SymTridiagonal(Δdv, Δev)
+end
+
+ChainRulesTestUtils.test_frule(SymTridiagonal, [0.1,0.2,0.3],ones(2))
