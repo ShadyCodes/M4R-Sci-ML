cfde1a48764160f9d48541ab47cfd31de711537f
Dharshan B <sdbkar23@gmail.com>
2022-04-08 21:17:26 +0100

    rrule attempt, 1 error in testing

--- SymTridiagonalRRules.jl
+++ SymTridiagonalRRules.jl
@@ -1,11 +1,5 @@
 using Zygote, LinearAlgebra, ChainRules, ChainRulesCore, ChainRulesTestUtils
 
-function ChainRulesCore.rrule(::Type{SymTridiagonal}, dv::AbstractVector, ev::AbstractVector)
-    SymTridiagonal_pullback(Δdv, Δev) = NoTangent(), SymTridiagonal(Δdv, Δev)
-    return SymTridiagonal(dv,ev), SymTridiagonal_pullback
-end
-
-
 #A = c -> Symmetric([c[1] c[1]; 0 c[2]+2c[3]]);
 #λ = c -> eigvals(A(c))
 
@@ -16,8 +10,20 @@ end
 #λ = c -> eigvals(A(c))
 #Zygote.jacobian(A, [0.1,0.2,0.3]
 
-function ChainRulesCore.frule((_, Δdv, Δev), ::Type{SymTridiagonal}, dv::AbstractVector, ev::AbstractVector)
+function ChainRulesCore.frule((_, Δdv, Δev), ::Type{SymTridiagonal}, dv::V, ev::V) where V <: AbstractVector
     return SymTridiagonal(dv,ev), SymTridiagonal(Δdv, Δev)
 end
 
 ChainRulesTestUtils.test_frule(SymTridiagonal, [0.1,0.2,0.3],ones(2))
+
+function ChainRulesCore.rrule(::Type{SymTridiagonal}, dv::V, ev::V) where V <: AbstractVector
+    Ω = SymTridiagonal(dv,ev)
+    function SymTridiagonal_pullback(ΔΩ)
+        Δdv = @thunk(diag(ΔΩ))
+        Δev = @thunk(diag(ΔΩ,1))
+        return (NoTangent(), Δdv, Δev)
+    end
+    return (Ω, SymTridiagonal_pullback)
+end
+
+test_rrule(SymTridiagonal, [0.1,0.2,0.3],ones(2))
